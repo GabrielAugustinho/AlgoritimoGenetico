d2235c64ca384008f1a7626b7b9aad512038c49b
Gabriel Augustinho <gabriel.aug.alves@outlook.com>
2023-08-16 12:19:45 -0300

    Adição da aptidao fitness.

--- AlgoritimoGenetico/Core/AlgoritimoGeneticoService.cs
+++ AlgoritimoGenetico/Core/AlgoritimoGeneticoService.cs
@@ -5,70 +5,131 @@ namespace AlgoritimoGenetico.Core
 {
     public class AlgoritimoGeneticoService : IAlgoritimoGeneticoService
     {
-        private readonly Random Random = new();
+        private readonly Random Aleatorio = new();
         private readonly int QtdRainhas = 8;
         private int TamanhoPopulacao;
         private HashSet<int[]> Populacao = new();
 
         public void Run()
         {
-            IniciarProcesso();
-            MostrarPopulacaoInicial();
+            InicializarPopulacao();
+            MostrarProbabilidadesDeSobrevivencia();
             // Implementar o restante da lógica do algoritmo genético aqui
+            Console.ReadLine();
         }
 
-        private void IniciarProcesso()
+        private void InicializarPopulacao()
         {
             CalcularTamanhoPopulacao();
-            CriarPopulacaoInicial();
+            GerarPopulacaoInicial();
+            MostrarPopulacaoInicial();
         }
 
         private void CalcularTamanhoPopulacao()
         {
-            // Verificar se o tamanho está ok
-            // TamanhoPopulacao = Enumerable.Range(1, QtdRainhas).Aggregate(1, (p, item) => p * item) / 2;
-            TamanhoPopulacao = 100;
+            TamanhoPopulacao = 10;
         }
 
-        private void CriarPopulacaoInicial()
+        private void GerarPopulacaoInicial()
         {
-            Populacao = GerarPopulacaoInicial();
+            Populacao = GerarConjuntoPopulacionalInicial();
         }
 
-        private HashSet<int[]> GerarPopulacaoInicial()
+        private HashSet<int[]> GerarConjuntoPopulacionalInicial()
         {
-            var populacaoInicial = new HashSet<int[]>(new ArrayComparer());
-            for (int i = 0; i <= TamanhoPopulacao; i++)
+            var conjuntoPopulacionalInicial = new HashSet<int[]>(new ArrayComparer());
+            for (int i = 0; i < TamanhoPopulacao; i++)
             {
                 var cromossomo = GerarCromossomoAleatorio();
-                while (!populacaoInicial.Add(cromossomo))
+                while (!conjuntoPopulacionalInicial.Add(cromossomo))
                 {
                     cromossomo = GerarCromossomoAleatorio();
                 }
             }
-            return populacaoInicial;
+            return conjuntoPopulacionalInicial;
         }
 
         private int[] GerarCromossomoAleatorio()
         {
-            var cromossomo = new int[QtdRainhas];
-            for (int i = 0; i < QtdRainhas; i++)
+            var cromossomo = Enumerable.Range(0, QtdRainhas).ToArray();
+            EmbaralharArray(cromossomo);
+            return cromossomo;
+        }
+
+        private void EmbaralharArray<T>(T[] array)
+        {
+            int n = array.Length;
+            for (int i = n - 1; i > 0; i--)
             {
-                cromossomo[i] = i;
+                int j = Aleatorio.Next(i + 1);
+                (array[j], array[i]) = (array[i], array[j]);
             }
-            return cromossomo.OrderBy(x => Random.Next()).ToArray(); // Embaralha
         }
 
         private void MostrarPopulacaoInicial()
         {
             Console.WriteLine("População inicial:\n");
-            foreach (var item in Populacao)
+            foreach (var individuo in Populacao)
             {
-                Console.WriteLine(JsonConvert.SerializeObject(item));
+                Console.WriteLine(JsonConvert.SerializeObject(individuo));
             }
-            Console.ReadLine();
         }
 
-        // Implementar outras funções relacionadas ao algoritmo genético
+        private double[] CalcularProbabilidadesDeSobrevivencia()
+        {
+            double totalAptidao = 0;
+            var aptidao = new double[Populacao.Count];
+            int index = 0;
+
+            foreach (var individuo in Populacao)
+            {
+                var colisoes = CalcularColisoes(individuo);
+                var aptidaoIndividuo = colisoes != 0 ? 1.0 / colisoes : 9999.0;
+
+                aptidao[index] = aptidaoIndividuo;
+                totalAptidao += aptidaoIndividuo;
+                index++;
+            }
+
+            for (int i = 0; i < aptidao.Length; i++)
+            {
+                aptidao[i] /= totalAptidao;
+            }
+
+            return aptidao;
+        }
+
+        private int CalcularColisoes(int[] individuo)
+        {
+            int n = individuo.Length;
+            int colisoes = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int xDistancia = Math.Abs(j - i);
+                    int yDistancia = Math.Abs(individuo[j] - individuo[i]);
+
+                    if (xDistancia == yDistancia)
+                    {
+                        colisoes++;
+                    }
+                }
+            }
+
+            return colisoes;
+        }
+
+        private void MostrarProbabilidadesDeSobrevivencia()
+        {
+            var aptidoesPopulacionais = CalcularProbabilidadesDeSobrevivencia();
+
+            Console.WriteLine("\nProbabilidades de sobrevivência:\n");
+            foreach (var aptidao in aptidoesPopulacionais)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(aptidao));
+            }
+        }
     }
 }
